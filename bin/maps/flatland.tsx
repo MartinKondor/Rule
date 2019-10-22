97924a0a2b0a2a3d8f41877b8355e66d1d8ff940
Martin Kondor <martinkondor@gmail.com>
2019-10-22 08:32:01 +0200

    Adding tiled map

---
+++ bin/maps/flatland.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.10.18" name="flatland" tilewidth="64" tileheight="64" tilecount="320" columns="16">
+ <image source="D:/Projects/Rule/bin/tilesets/basic-64x64.png" width="1024" height="1280"/>
+</tileset>
